2cc8833ac57a95c4cc9f471435cdbcb2bf5b0c7d
att124 <daniel.fernandez@davinci.edu.ar>
2024-11-26 14:14:06 -0300

    Cards del dashboard de admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dashboard from './components/admin/panel_admin/Dashboar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/components/admin/cards/card.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import './card.css';
+import { Link } from 'react-router-dom';
+
+
+
+const cardData = [{
+
+imgSrc: "https://fakeimg.pl/300x300/?text=1",
+alttxt: "Gestion de usuarios",
+titulo: "Gestion de usuarios",
+descripcion: "Administra todos los usuarios registrados en el sistema",
+link: "/admin/users"
+}, {
+
+imgSrc: "https://fakeimg.pl/300x300/?text=1",    
+alttxt: "Gestion de canciones",
+titulo: "Gestion de canciones",
+descripcion: "Administra todas la canciones disponibles en la plataforma",
+link: "/admin/songs"
+}]
+
+
+
+const Card = () => 
+{
+
+    return (
+        
+        <div className="contenedor-card">
+
+              {
+              cardData.map((card, indice) => (
+
+                <div className="card" key={indice}>
+
+                    <img src={card.imgSrc} alt={card.alttxt} className="card-img"/>
+
+                    <div className="card-content">
+
+                        <h2>{card.titulo}</h2>
+
+                        <p>{card.descripcion}</p>
+
+                        <Link to={card.link} className="cbutton">ir a {card.titulo}</Link>
+
+                    </div>
+
+
+                </div>
+            ))}
+                
+                
+
+
+
+
+        </div>
+
+
+
+    );
+    
+
+};
+
+
+
+
+export default Card;
