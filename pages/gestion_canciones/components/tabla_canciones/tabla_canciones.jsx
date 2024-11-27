9c0bc809280a49ac22f260ff058353042328792f
att124 <daniel.fernandez@davinci.edu.ar>
2024-11-27 13:26:37 -0300

    Cambios

--- pages/gestion_canciones/components/tabla_canciones/tabla_canciones.jsx
+++ pages/gestion_canciones/components/tabla_canciones/tabla_canciones.jsx
@@ -1,57 +1,54 @@
-
-
 import React from "react";
 import FilaCancion from '../filas_tabla_canciones/filas_canciones';
 import './tabla_canciones.css';
-import { songs } from '../../../../src/data/data';
+import { songs, artists } from '../../../../src/data/data';
 
 const TablaCancion = () => {
 
-return (
+    // metodo para obtener los nombres de los artistas
+   
+    const getArtistNames = (artistIds) => {
+        return artistIds.map(id => {
+            const artist = artists.find(artist => artist.id === id);
+            return artist ? artist.name : 'No se encontró el artista'; // Si no se encuentra el artista, devuelve 'No se encontró el artista'
+        }).join(', '); // Unir los nombres de los artistas con coma
+    };
 
-<div className="tabla-padding">
-
-
-<table className="tabla-de-canciones"> 
-
-
-<thead>
-            <tr>
-
+    return (
+        <div className="tabla-padding">
+            <table className="tabla-de-canciones"> 
+                <thead>
+                    <tr>
                         <th className="centrar-th-tabla">Id</th>
                         <th className="centrar-th-tabla">Imágen</th>
                         <th className="centrar-th-tabla">Canción</th>
                         <th className="centrar-th-tabla">Artista</th>
                         <th className="centrar-th-tabla">Acciones</th>
+                    </tr>
+                </thead>
 
-            </tr>
-
-</thead>
-
-<tbody>
-
-{
-    
-    songs.map(song => ( <FilaCancion key={song.id} id={song.id} nombre={song.name} artista={song.artist} imagen={song.image} /> ))
-    
-}
-
-</tbody>
-
-
-</table>
-
-</div>
-
-
-
-
-
-
-);
-
-
-
-}
+                <tbody>
+                    {
+                        
+                        songs.map(song => {
+                           
+                            const artistNames = getArtistNames(song.artistIds);
+                            
+                            return (
+                                <FilaCancion 
+                                    key={song.id} 
+                                    id={song.id} 
+                                    nombre={song.name} 
+                                    artista={artistNames} 
+                                    imagen={song.image} 
+                                />
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+};
 
 export default TablaCancion;
