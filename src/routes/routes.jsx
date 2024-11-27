8c325255173ea567b62ee4f76724640a2c094e35
Agustin Aguero <agustin.aguero@davinci.edu.ar>
2024-11-26 21:30:13 -0300

    fix routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,7 @@
 import { Home } from "../../pages/Home";
 import Dashboard from "../components/admin/panel_admin/Dashboard";
 import { RecentsReleases } from "../Components/RecentsRelease/RecentsRelease";
-Home
+
 export const rutas = [
     { path:"/", element: < Home />, name: "Home" },
     { path:"/Dashboard", element: <Dashboard />, name: "Panel Artista"}
