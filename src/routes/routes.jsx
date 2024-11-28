7f3de303973bae6d757a9ebd4eb9711d8d10b626
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-11-28 00:39:41 -0300

    Update routes.jsx

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -14,21 +14,42 @@ import Register from "../Components/Auth/Register";
 import EditProfile from "../Components/Profile-todo/EditProfile";
 import Profile from "../Components/Profile-todo/Profile";
 
+
+import EditarCancion from "../../pages/editar_Canciones/EditarCancion";
+import { Gestion_Albumes } from "../../pages/gestion_albumes/Gestion_Albumes";
+import { Generos } from "../../pages/Generos";
+import { GlobalRanking } from "../../pages/GlobalRanking";
+import { Search } from "../../pages/Search";
+
+
 export const rutas = [
     { path:"/", element: < Home />, name: "Home" },
     { path:"/Dashboard", element: <Dashboard />, name: "Panel Artista"},
     { path: "/Dashboard/Gestion_Canciones", element: <Gestion_Canciones />, name: "Panel canciones" },
+    {path: "/Dashboard/Gestion_Canciones/EditarCancion/:id", element: <EditarCancion />, name: "Editar Cancion",},
+    { path: "/Dashboard/Gestion_Albumes", element: <Gestion_Albumes />, name: "Panel albumes" },
 
 
-        //Autentificacion
+    // Genero
+    { path: "/generos/:id", element: <Generos />, name: "Lista Genero" },
 
-        { path: "/login", element: <LoginPage />, name: "Login" }, 
-        { path: "/register", element: <RegisterPage />, name: "Register" },
-        { path: "/profile", element: <Profile />, name: "Profile" },
-        { path: "/edit-profile", element: <EditProfile />, name: "Edit Profile" },
+
+    // Listas
+    {path: "/globalRanking", element: <GlobalRanking />, name: "Ranking Global",},
+
+
+    //Autentificacion
+    { path: "/login", element: <LoginPage />, name: "Login" }, 
+    { path: "/register", element: <RegisterPage />, name: "Register" },
+    { path: "/profile", element: <Profile />, name: "Profile" },
+    { path: "/edit-profile", element: <EditProfile />, name: "Edit Profile" },
     
 
     //Artistas
     { path: "/artist/:id", element: <ArtistProfile />, name: "Perfil del Artista" },
+
+
+    // Buscador
+    { path: "/search", element: <Search />, name: "Search" },
     
     ]
