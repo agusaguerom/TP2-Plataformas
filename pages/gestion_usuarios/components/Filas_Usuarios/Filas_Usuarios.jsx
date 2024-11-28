bbb6bfa8602f85c3d15b9c848dee4db3d8aa8e61
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-11-28 06:14:36 -0300

    usuarios mostrados por local storage

--- pages/gestion_usuarios/components/Filas_Usuarios/Filas_Usuarios.jsx
+++ pages/gestion_usuarios/components/Filas_Usuarios/Filas_Usuarios.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./Filas_Usuarios.css";
 
 const FilasUsuarios = ({ id, nombre, email, rol }) => {
-    return (
-        <tr>
-            <td className="contenido-txt-usuarios">{id}</td>
-            <td className="contenido-txt-usuarios">{nombre}</td>
-            <td className="contenido-txt-usuarios">{email}</td>
-            <td className="contenido-txt-usuarios">{rol}</td>
-            <td className="contenido-txt-usuarios">
-                <button className="btn btn-warning btn-deshabilitar">Modificar</button>
-                <button className="btn btn-danger btn-deshabilitar">Deshabilitar</button>
-            </td>
-        </tr>
-    );
-}
+  return (
+    <tr>
+      <td className="contenido-txt-usuarios">{id}</td>
+      <td className="contenido-txt-usuarios">{nombre}</td>
+      <td className="contenido-txt-usuarios">{email}</td>
+      <td className="contenido-txt-usuarios">{rol}</td>
+      <td className="contenido-txt-usuarios">
+        <button className="btn btn-warning btn-deshabilitar">Modificar</button>
+        <button className="btn btn-danger btn-deshabilitar">Deshabilitar</button>
+      </td>
+    </tr>
+  );
+};
 
 export default FilasUsuarios;
--- pages/gestion_usuarios/components/Tabla_Usuarios/tabla_usuarios.jsx
+++ pages/gestion_usuarios/components/Tabla_Usuarios/tabla_usuarios.jsx
@@ -1,29 +1,41 @@
-import React from "react";
-import { users } from '../../../../src/data/data';  
+import React, { useState, useEffect } from "react";
 import FilasUsuarios from "../Filas_Usuarios/Filas_Usuarios";
 import './Tabla_Usuarios.css';
 
 const TablaUsuarios = () => {
-    return(
-        <div className="tabla-padding-usuarios">
-            <table className="tabla-de-usuarios">
-                <thead>
-                    <tr>
-                        <th className="centrar-th-usuarios">Id</th>
-                        <th className="centrar-th-usuarios">Nombre</th>
-                        <th className="centrar-th-usuarios">Email</th>
-                        <th className="centrar-th-usuarios">Rol</th>
-                        <th className="centrar-th-usuarios">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map(user => (
-                        <FilasUsuarios key={user.id} id={user.id} nombre={user.name} email={user.email} rol={user.role} />
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-}
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
+    setUsers(storedUsers);
+  }, []);
+
+  return (
+    <div className="tabla-padding-usuarios">
+      <table className="tabla-de-usuarios">
+        <thead>
+          <tr>
+            <th className="centrar-th-usuarios">Id</th>
+            <th className="centrar-th-usuarios">Nombre</th>
+            <th className="centrar-th-usuarios">Email</th>
+            <th className="centrar-th-usuarios">Rol</th>
+            <th className="centrar-th-usuarios">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <FilasUsuarios
+              key={user.id}
+              id={user.id}
+              nombre={user.username}
+              email={user.email}
+              rol={user.role}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default TablaUsuarios;
