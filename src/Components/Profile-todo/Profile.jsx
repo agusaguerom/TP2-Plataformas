b102cd97b1947c26f4fdda9fe2a9fd2e08d9224a
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-11-28 07:05:22 -0300

    no pude acomodar el boton

--- src/Components/Profile-todo/Profile.jsx
+++ src/Components/Profile-todo/Profile.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
-
 import '../../../pages/ProfilePage/ProfilePage.css';
 
 const Profile = () => {
