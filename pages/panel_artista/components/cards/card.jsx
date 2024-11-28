8743f83041628c88993cb5711fff7bd395721319
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-11-28 02:19:01 -0300

    cambios en dashboard si es artist o admin

--- pages/panel_artista/components/cards/card.jsx
+++ pages/panel_artista/components/cards/card.jsx
@@ -1,76 +1,86 @@
-// Card.jsx
 import React from 'react';
 import './card.css';
 import { Link } from 'react-router-dom';
 import Contador from '../contadores/contador';
 import { useState, useEffect } from 'react';
 
+const Card = ({ admin }) => {
+  const [cantidadSeguidores, setCantidadSeguidores] = useState(0);
+  const [cantidadCanciones, setCantidadCanciones] = useState(0);
 
+  useEffect(() => {
+    setCantidadSeguidores(1200);
+    setCantidadCanciones(56);
+  }, []);
 
-const Card = () => {
-
-    const [cantidadSeguidores, setCantidadSeguidores] = useState(0);
-    const [cantidadCanciones, setCantidadCanciones] = useState(0);
-    
-    useEffect(() => {
-    
-        setCantidadSeguidores(1200);  
-        setCantidadCanciones(56);    
-    
-      }, []);
-    
-    
-    const cardData = [
-      {
-        tipo: "info",
-        titulo: "Cantidad de Seguidores:",
-        valor: cantidadSeguidores
-      },
-      {
-        tipo: "info",
-        titulo: "Cantidad de Canciones:",
-        valor: cantidadCanciones
-      },
-      {
-        tipo: "gestion",
-        imgSrc: "https://fakeimg.pl/300x300/?text=1",
-        alttxt: "Gestion de usuarios",
-        titulo: "Configuración de Perfil",
-        descripcion: "Modificar perfil",
-        link: "/admin/users"
-      },
-      {
-        tipo: "gestion",
-        imgSrc: "/ondas.png",
-        alttxt: "Gestion de canciones",
-        titulo: "Gestion de canciones",
-        descripcion: "Administrar mis canciones",
-        link: "/Dashboard/Gestion_Canciones"
-      },
-      {
-        tipo: "gestion",
-        imgSrc: "/album.jpg",
-        alttxt: "Gestión de álbumes",
-        titulo: "Gestion de álbumes",
-        descripcion: "Administrar mis álbumes",
-        link: "/Dashboard/Gestion_Albumes"
-      }
-    ];
-
+  const cardData = admin
+    ? [
+        {
+          tipo: "gestion",
+          imgSrc: "https://fakeimg.pl/300x300/?text=1",
+          alttxt: "Gestión de perfil",
+          titulo: "Configuración de Perfil",
+          descripcion: "Modificar perfil",
+          link: "/profile"
+        },
+        {
+          tipo: "gestion",
+          imgSrc: "https://fakeimg.pl/300x300/?text=2",
+          alttxt: "Gestión de usuarios",
+          titulo: "Administrar Usuarios",
+          descripcion: "Administrar usuarios",
+          link: "/admin/users/manage"
+        }
+      ]
+    : [
+        {
+          tipo: "info",
+          titulo: "Cantidad de Seguidores:",
+          valor: cantidadSeguidores
+        },
+        {
+          tipo: "info",
+          titulo: "Cantidad de Canciones:",
+          valor: cantidadCanciones
+        },
+        {
+          tipo: "gestion",
+          imgSrc: "/ondas.png",
+          alttxt: "Gestión de canciones",
+          titulo: "Gestión de canciones",
+          descripcion: "Administrar mis canciones",
+          link: "/Dashboard/Gestion_Canciones"
+        },
+        {
+          tipo: "gestion",
+          imgSrc: "/album.jpg",
+          alttxt: "Gestión de álbumes",
+          titulo: "Gestión de álbumes",
+          descripcion: "Administrar mis álbumes",
+          link: "/Dashboard/Gestion_Albumes"
+        },
+        {
+          tipo: "gestion",
+          imgSrc: "https://fakeimg.pl/300x300/?text=1",
+          alttxt: "Gestión de perfil",
+          titulo: "Configuración de Perfil",
+          descripcion: "Modificar perfil",
+          link: "/admin/users"
+        }
+      ];
 
   return (
     <div className="contenedor-card">
-    <div className="card-row">
-      {cardData.filter(card => card.tipo === "info").map((card, indice) => (
-        <div key={indice}>
-          <Contador
-            titulo={card.titulo}
-            valor={card.valor}
-          />
-        </div>
-      ))}
-    </div>
-
+      <div className="card-row">
+        {cardData.filter(card => card.tipo === "info").map((card, indice) => (
+          <div key={indice} >
+            <Contador
+              titulo={card.titulo}
+              valor={card.valor}
+            />
+          </div>
+        ))}
+      </div>
       <div className="card-row">
         {cardData.filter(card => card.tipo === "gestion").map((card, indice) => (
           <div className="custom-card" key={indice}>
--- pages/panel_artista/panel_admin/Dashboard.jsx
+++ pages/panel_artista/panel_admin/Dashboard.jsx
@@ -1,24 +1,27 @@
-
-import React from "react"
+import React from "react";
 import Card from '../components/cards/card';
 import './Dashboard.css';
+import { useAuth } from '../../../src/context/AuthContext';
+
 const Dashboard = () => {
+  const { userLogueado } = useAuth();
 
-return (
-
+  return (
     <>
-
-            <h1 className="titulo_Dashboard">Panel del Artista</h1>
-
-            <h2 className="subtitulo_Dashboard">Seleccionar categoria</h2>
-
-            <Card />
-
+      {userLogueado.role === 'admin' ? (
+        <>
+          <h1 className="titulo_Dashboard">Panel del Admin</h1>
+          <Card admin />
+        </>
+      ) : (
+        <>
+          <h1 className="titulo_Dashboard">Panel del Artista</h1>
+          <h2 className="subtitulo_Dashboard">Seleccionar categoría</h2>
+          <Card />
+        </>
+      )}
     </>
-
-
-);
-
+  );
 };
 
 export default Dashboard;
