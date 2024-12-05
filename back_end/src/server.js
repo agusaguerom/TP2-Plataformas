6eb2e10cb1a29675490d82a6f66720f12b3332f5
att124 <daniel.fernandez@davinci.edu.ar>
2024-12-05 15:27:49 -0300

    express

---
+++ back_end/src/server.js
@@ -0,0 +1,44 @@
+
+import express, { response } from "express";
+
+const app = express(); // Inicializa la aplicacion
+
+const PORT = 5000; // puerto
+
+app.get("/", (req, resp) => {
+
+resp.json({mensaje: "hola mundo",
+fecha: new Date().toLocaleDateString(),
+cantidad: 150
+
+
+
+});
+
+}) // Ruta raiz
+
+
+app.get("/info", (req, res) => {
+
+res.json({
+
+    mensaje: "Información del servidor",
+    version: "0.1.0",
+    auto: "Pablito",
+    framework: "Express",
+
+
+});
+
+
+
+});
+
+
+
+
+app.listen(PORT, () => {
+
+console.log(`Server running on http://localhost:${PORT}`);
+
+})
