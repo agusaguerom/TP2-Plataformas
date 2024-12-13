02d579aff16c6ce5da797ab17d67bda07bb61fe2
Agustin Aguero <agustin.aguero@davinci.edu.ar>
2024-12-12 22:41:36 -0300

    a

--- front_end/pages/gestion_albumes/components/Filas_Albumes/filas_albumes.jsx
+++ front_end/pages/gestion_albumes/components/Filas_Albumes/filas_albumes.jsx
@@ -1,35 +1,29 @@
 import React from "react";
 import "./filas_albumes.css";
 import { Link } from "react-router-dom";
-const Filas_Albumes = ({id, publicacion, nombre,descripcion, artista}) => {
-{/*className = "contenido-centrado-tabla-album", Esto es para las imagenes */}
-return (
-
-    
+const Filas_Albumes = ({ id, publicacion, nombre, descripcion, artista }) => {
+  {
+    /*className = "contenido-centrado-tabla-album", Esto es para las imagenes */
+  }
+  return (
     <tr>
-
-        <td className="contenido-txt-album">{id}</td>
-        <td className="contenido-txt-album">{publicacion}</td> 
-        <td className="contenido-txt-album">{nombre}</td>
-        <td className="contenido-txt-album">{descripcion}</td>
-        <td className="contenido-txt-album">{artista}</td>
-        <td  className="contenido-txt-album">
-
+      <td className="contenido-txt-album">{id}</td>
+      <td className="contenido-txt-album">{publicacion}</td>
+      <td className="contenido-txt-album">{nombre}</td>
+      <td className="contenido-txt-album">{descripcion}</td>
+      <td className="contenido-txt-album">{artista}</td>
+      <td className="contenido-txt-album">
         <div className="Centrar-botones-album">
-
-        <Link to={`/Dashboard/Gestion_Albumes/EditarAlbum/${id}`} className="btn btn-warning Modificar-boton">
-                        Modificar
-                    </Link>
-        <button className="btn btn-danger btn-deshabilitar">Deshabilitar</button>
-
+          <Link
+            to={`/Dashboard/Gestion_Albumes/EditarAlbum/${id}`}
+            className="btn btn-warning Modificar-boton"
+          >
+            Modificar
+          </Link>
         </div>
-
-        </td>
-</tr>
-
-)
-
-
-}
+      </td>
+    </tr>
+  );
+};
 
 export default Filas_Albumes;
