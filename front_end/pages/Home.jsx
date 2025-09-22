e80fc6a2103bef7beebf73afa45f7c6db37a9480
agusaguerom <agusaguerom28@gmail.com>
2025-09-22 15:26:47 -0300

    import fix generos

--- front_end/pages/Home.jsx
+++ front_end/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { RecentsReleases } from "../src/Components/RecentsRelease/RecentsRelease";
+import { RecentsReleases } from "../src/components/RecentsRelease/RecentsRelease";
 import { GlobalRanking } from "../src/components/GlobalRanking/GlobalRanking";
 import { GenerosList } from "../src/components/GenerosList/GenerosList";
 import { useRef } from "react";
