a820b2a4b606c45d7e7d7f76a68d96ddf849785f
agusaguerom <agusaguerom28@gmail.com>
2025-09-22 15:23:37 -0300

    import fix2

--- front_end/src/routes/routes.jsx
+++ front_end/src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import Home from "../../pages/Home";
 import Dashboard from "../../pages/panel_artista/panel_admin/Dashboard";
-import { RecentsReleases } from "../Components/RecentsRelease/RecentsRelease";
+import { RecentsReleases } from "../components/RecentsRelease/RecentsRelease";
 import Gestion_Canciones from "../../pages/gestion_canciones/Gestion_Canciones";
 import ArtistProfile from "../../pages/ArtistProfile";
 
@@ -9,10 +9,10 @@ import RegisterPage from "../../pages/RegisterPage/Register";
 import ProfilePage from "../../pages/ProfilePage/Profile";
 import EditProfilePage from "../../pages/ProfilePage/EditProfile";
 
-import Login from "../Components/Auth/Login";
-import Register from "../Components/Auth/Register";
-import EditProfile from "../Components/Profile-todo/EditProfile";
-import Profile from "../Components/Profile-todo/Profile";
+import Login from "../components/Auth/Login";
+import Register from "../components/Auth/Register";
+import EditProfile from "../components/Profile-todo/EditProfile";
+import Profile from "../components/Profile-todo/Profile";
 
 import EditarCancion from "../../pages/editar_Canciones/EditarCancion";
 import Gestion_Albumes from "../../pages/gestion_albumes/Gestion_Albumes";
